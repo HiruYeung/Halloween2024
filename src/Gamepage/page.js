dfbbd32455f818da9d82207714634316823b14fb
hiru <ehiruac@gmail.com>
2024-11-01 22:33:35 +0800

    up

--- src/Gamepage/page.js
+++ src/Gamepage/page.js
@@ -19,7 +19,7 @@ const Page = () => {
   return (
     <>
       <div style={{ position: 'relative' }}>
-        <div className='scroll-thumb2' style={{ display: 'flex', gap: '0', position: 'absolute',}}>Ehiru's Mini Web Game</div>
+        <div className='scroll-thumb2' style={{ display: 'flex', gap: '0', position: 'absolute',}}>Ehiru's Mini Web Game<br/>Instagram:art.ehiru/Email:ehiruac@gmail.com</div>
         <img src={Logoimg} className="logo" alt="Logo" style={{ display: 'flex', gap: '0', width: '100%'}}/>
         <div className='Plays' style={{ display: 'flex', gap: '0',}}>
           <img src={Blue1} className="Blue1" style={{ display: 'flex', gap: '0', width:'27%'}}/>
